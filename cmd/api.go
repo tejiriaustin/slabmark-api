92a92fc6a614db770eb72cbc55c3b15b2f66d6ee
hs-tejiri-austin <tejiri.austin@heroshe.com>
2023-07-28 20:39:57 +0100

    SB: setup router and response mechanism

--- cmd/api.go
+++ cmd/api.go
@@ -1,1 +1,19 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+// apiCmd represents the api command
+var apiCmd = &cobra.Command{
+	Use:   "api",
+	Short: "Starts slab-marks api",
+	Long:  ``,
+	Run:   startApi,
+}
+
+func init() {
+	rootCmd.AddCommand(apiCmd)
+}
+
+func startApi(cmd *cobra.Command, args []string) {
+
+}
